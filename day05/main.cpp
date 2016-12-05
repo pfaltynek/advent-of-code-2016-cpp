eb648660e7a723cedba7c95d486d88cfd88baf5f
Petr Faltýnek <pfaltynek@gmail.com>
2016-12-05 21:49:19 +0100

    added simple password decoding animation for day 5

--- day05/main.cpp
+++ day05/main.cpp
@@ -3,10 +3,13 @@
 #include <openssl/md5.h>
 #include <string.h>
 #include <string>
+# include <ctime>
 
 char buffer[101];
 unsigned char hash[MD5_DIGEST_LENGTH];
 const std::string input = "abbhdwsy";
+const std::string hour_glass = "\\|/-";
+const char unknown_char = '*';
 
 char HalfByteToHex(int val) {
 	char c;
@@ -24,13 +27,20 @@ char HalfByteToHex(int val) {
 int main(void) {
 	std::string result1, result2;
 	int number = 0, size, val1, val2;
+	int hour_glass_idx = 0;
 	bool finished = false;
+	bool update_console = false;
+	clock_t current_time, prev_time = clock();
 
+	std::cout << std::endl;
 	std::cout << "=== Advent of Code 2016 - day 5 ====" << std::endl;
-	std::cout << "--- part 1 ---" << std::endl;
+	std::cout << "Decoding passwords:" << std::endl;
 
 	result1.clear();
-	result2 = "________";
+	result2.clear();
+	for(int i = 0; i < 8; i++){
+		result2.push_back(unknown_char);
+	}
 
 	while (!finished) {
 		number++;
@@ -43,19 +53,38 @@ int main(void) {
 				val2 = hash[3] / 0x10;
 				if (result1.size() < 8) {
 					result1.push_back(HalfByteToHex(val1));
+					update_console = true;
 				}
 				if (val1 < 8) {
-					if (result2[val1] == '_') {
+					if (result2[val1] == unknown_char) {
 						result2[val1] = HalfByteToHex(val2);
+						update_console = true;
 					}
 				}
 			}
 		}
+		current_time = clock();
+		if ((current_time - prev_time) > (CLOCKS_PER_SEC/4)){
+			hour_glass_idx += ((current_time - prev_time) / (CLOCKS_PER_SEC/4));
+			hour_glass_idx %= hour_glass.size();
+			prev_time = current_time;
+			update_console = true; 
+		}
+		if (update_console) {
+			std::cout << "\r  ";
+			std::cout << hour_glass[hour_glass_idx];
+			std::cout << "  part 1: ";
+			std::cout << result1;
+			for (int i = result1.size(); i < 8; i++) {
+				std::cout << unknown_char;
+			}
+			std::cout << "    part 2: " << result2;
+			std::cout << "  " << hour_glass[hour_glass_idx] << std::flush;
+			update_console = false;
+		}
 		finished =
-			(result1.size() == 8) && (result2.find('_') == std::string::npos);
+			(result1.size() == 8) && (result2.find(unknown_char) == std::string::npos);
 	}
 
-	std::cout << "Result is " << result1 << std::endl;
-	std::cout << "--- part 2 ---" << std::endl;
-	std::cout << "Result is " << result2 << std::endl;
+	std::cout << std::endl << std::endl << "Decoding finished." << std::endl;
 }
