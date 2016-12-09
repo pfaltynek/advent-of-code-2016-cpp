6eee429db7b7b3167983ff3089bc5ce9233678ec
Petr Faltýnek <pfaltynek@gmail.com>
2016-12-09 09:54:21 +0100

    finished solution of day 8

--- day08/main.cpp
+++ day08/main.cpp
@@ -1,7 +1,9 @@
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
 
+#define DEBUG_OUTPUT 0
 const std::string col_rotate = "rotate column x=";
 const std::string row_rotate = "rotate row y=";
 const std::string rect = "rect ";
@@ -9,7 +11,7 @@ const std::string row_col_divier = " by ";
 const std::string rect_divier = "x";
 const int MAX_COLUMN = 50;
 const int MAX_ROW = 6;
-#define DEBUG_OUTPUT 1 
+
 unsigned char display[MAX_COLUMN][MAX_ROW];
 
 void ShowDisplay(void) {
@@ -26,13 +28,13 @@ void ShowDisplay(void) {
 int main(void) {
 	std::ifstream input;
 	std::string line;
-	int result1, result2, cnt;
+	int result1, cnt;
 
 	std::cout << "=== Advent of Code 2016 - day 8 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
 
 	input.open("input.txt", std::ifstream::in);
-	//input.open("input-test.txt", std::ifstream::in);
+	// input.open("input-test.txt", std::ifstream::in);
 
 	if (input.fail()) {
 		std::cout << "Error opening input file.\n";
@@ -41,16 +43,22 @@ int main(void) {
 
 	cnt = 0;
 	result1 = 0;
-	result2 = 0;
 	memset(display, sizeof(display), 0);
-	#if DEBUG_OUTPUT
+
+#if DEBUG_OUTPUT
 	ShowDisplay();
-	#endif
+#endif
+
 	while (std::getline(input, line)) {
 		std::string part, l, r;
 		int pos, v1, v2, tmp;
 
 		cnt++;
+
+#if DEBUG_OUTPUT
+		std::cout << "Processing line " << cnt << ": " << line << std::endl;
+#endif
+
 		if (line.find(row_rotate) == 0) {
 			part = line.substr(row_rotate.size());
 			pos = part.find(row_col_divier);
@@ -69,7 +77,7 @@ int main(void) {
 			}
 			v1 = atoi(l.c_str());
 			v2 = atoi(r.c_str());
-			if ((v1 >= MAX_COLUMN) || (v1 < 0) || (v2 < 0)) {
+			if ((v1 >= MAX_ROW) || (v1 < 0) || (v2 < 0)) {
 				std::cout
 					<< "Invalid row rotation instruction parameters at line "
 					<< cnt << std::endl;
@@ -101,7 +109,7 @@ int main(void) {
 			}
 			v1 = atoi(l.c_str());
 			v2 = atoi(r.c_str());
-			if ((v1 >= MAX_ROW) || (v1 < 0) || (v2 < 0)) {
+			if ((v1 >= MAX_COLUMN) || (v1 < 0) || (v2 < 0)) {
 				std::cout
 					<< "Invalid column rotation instruction parameters at line "
 					<< cnt << std::endl;
@@ -145,7 +153,10 @@ int main(void) {
 			std::cout << "Unknown instruction at line " << cnt << std::endl;
 			return -1;
 		}
+
+#if DEBUG_OUTPUT
 		ShowDisplay();
+#endif
 	}
 
 	if (input.is_open()) {
@@ -162,5 +173,9 @@ int main(void) {
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
-	std::cout << "Result is " << result2 << std::endl;
+	std::cout << "Result is:" << std::endl;
+
+#if !DEBUG_OUTPUT
+	ShowDisplay();
+#endif
 }
