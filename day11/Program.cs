23c9f38b25c4c39e36b505a24ed098c035684150
Faltynek Petr <FALTYNEK@zakladni.local>
2017-02-13 22:58:09 +0100

    started reimplementation of JAVA/C# solution for better preformance

---
+++ day11/Program.cs
@@ -0,0 +1,147 @@
+﻿// rewrite of JAVA code from https://github.com/exoji2e/aoc16-language-challenge/blob/master/11-java/A.java
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC_day11 {
+	class Program {
+		private class FuncCall {
+			public int[] n;
+			public int k;
+			public int e;
+			public FuncCall(int[] n, int k, int e) {
+				this.n = new int[n.Length];
+				for (int i = 0; i < n.Length; i++) {
+					this.n[i] = n[i];
+				}
+				this.k = k;
+				this.e = e;
+			}
+		}
+
+		private class Pair : IComparable<Pair> {
+			public int x, y;
+			public Pair(int x, int y) {
+				this.x = x; this.y = y;
+			}
+
+			public int CompareTo(Pair p) {
+				if (x != p.x) {
+					return x - p.x;
+				}
+				return y - p.y;
+			}
+		}
+
+		private static int DoIt(int[] n, out long time) {
+			int result = -1;
+			System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
+			HashSet<string> map = new HashSet<string>();
+			StringBuilder sb = new StringBuilder();
+			String finalstate;
+			List<FuncCall> bfs = new List<FuncCall>();
+			String s;
+
+			for (int i = 0; i <= n.Length; i++) {
+				sb.Append('3');
+			}
+			finalstate = sb.ToString();
+			bfs.Add(new FuncCall(n, 0, 0));
+			while (bfs.Count > 0) {
+				FuncCall f = bfs[0];
+				bfs.RemoveAt(0);
+				if (f.e < 0 || f.e > 3) continue; //elevator outside building
+				sortx(f.n);
+				if (!ok(f.n)) continue; //microship gets fried
+				s = getStr(f.n, f.e);
+				if (map.Contains(s)) continue; //state we've already been to.
+				map.Add(s);
+				if (s.Equals(finalstate)) { //found final state!
+					result = f.k;
+					break;
+				}
+
+
+				for (int i = 0; i < f.n.Length; i++) {
+					if (f.n[i] != f.e) continue;
+					f.n[i]--;
+					bfs.Add(new FuncCall(f.n, f.k + 1, f.e - 1)); //move n[i]
+					bool mvnxt = i % 2 == 0 && f.n[i + 1] == f.e;
+					if (mvnxt) {
+						f.n[i + 1]--;
+						bfs.Add(new FuncCall(f.n, f.k + 1, f.e - 1)); //move n[i]
+						f.n[i + 1]++;
+					}
+					f.n[i] += 2;
+					bfs.Add(new FuncCall(f.n, f.k + 1, f.e + 1)); //move n[i]
+					if (mvnxt) {
+						f.n[i + 1]++;
+						bfs.Add(new FuncCall(f.n, f.k + 1, f.e + 1)); //move n[i]
+						f.n[i + 1]--;
+					}
+					f.n[i]--;
+
+					for (int j = i + 2; j < f.n.Length; j += 2) {
+						if (f.n[j] != f.e) continue;
+						f.n[i]--; f.n[j]--;
+						bfs.Add(new FuncCall(f.n, f.k + 1, f.e - 1)); //move n[i]&n[j]
+						f.n[i] += 2; f.n[j] += 2;
+						bfs.Add(new FuncCall(f.n, f.k + 1, f.e + 1)); //move n[i]&n[j]
+						f.n[i]--; f.n[j]--;
+					}
+				}
+			}
+
+			stopwatch.Stop();
+			time = stopwatch.ElapsedMilliseconds;
+			return result;
+		}
+
+		public static void sortx(int[] n) {
+			Pair[] pairs = new Pair[n.Length / 2];
+			for (int i = 0; i < pairs.Length; i++) {
+				pairs[i] = new Pair(n[2 * i], n[2 * i + 1]);
+			}
+			Array.Sort(pairs);
+			for (int i = 0; i < pairs.Length; i++) {
+				n[2 * i] = pairs[i].x;
+				n[2 * i + 1] = pairs[i].y;
+			}
+		}
+
+		public static bool ok(int[] n) {
+			for (int i = 0; i < n.Length; i += 2) //microships
+				if (n[i] != n[i + 1])
+					for (int j = 1; j < n.Length; j += 2) //generator
+						if (n[j] == n[i])  //microship getting fried. 
+							return false;
+
+			return true;
+		}
+
+		public static String getStr(int[] n, int e) {
+			StringBuilder sb = new StringBuilder();
+			sb.Append(e);
+			for (int i = 0; i < n.Length; i++) {
+				sb.Append(n[i]);
+			}
+			return sb.ToString();
+		}
+
+		static void Main(string[] args) {
+			int x = 0, x1 = 0, x2 = 0;
+			long time = 0, t1 = 0, t2 = 0;
+
+			Console.WriteLine("=== Advent of Code 2016 - day 11 ====");
+			x = DoIt(new int[] { 0, 1, 0, 2 }, out time);
+			Console.WriteLine("--- test ---");
+			Console.WriteLine("Result is: {0} ({1}ms)", x, time);
+			x1 = DoIt(new int[] { 0, 0, 2, 1, 2, 1, 2, 1, 2, 1 }, out t1);
+			Console.WriteLine("--- part 1 ---");
+			Console.WriteLine("Result is: {0} ({1}ms)", x1, t1);
+			x2 = DoIt(new int[] { 0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 0, 0, 0, 0 }, out t2);
+			Console.WriteLine("--- part 2 ---");
+			Console.WriteLine("Result is: {0} ({1}ms)", x2, t2);
+		}
+	}
+}
--- day11/main.cpp
+++ day11/main.cpp
@@ -1,283 +1,54 @@
 
 #include <algorithm>
+#include <chrono>
 #include <cstring>
 #include <iostream>
-#include <vector>
-
 #include <queue>
+#include <vector>
 
 #define FIRST_FLOOR 0
 #define FINAL_FLOOR 3
 
 #define GENERATOR 0
 #define MICROCHIP 1
-const std::string type_names[] = {"G", "M"};
-const std::string empty_type_name = ".";
-const std::string empty_name = ".";
-
-#define MAX_RTG_TYPES 7
-#define MAX_RTG_TYPES_TEST 2
-#define MAX_RTG_TYPES_PART1 5
-#define MAX_RTG_TYPES_PART2 7
-const int init_map_test[MAX_RTG_TYPES_TEST][2] = {{FIRST_FLOOR + 1, FIRST_FLOOR}, {FIRST_FLOOR + 2, FIRST_FLOOR}};
-const int init_map_part1[MAX_RTG_TYPES_PART1][2] = {{FIRST_FLOOR, FIRST_FLOOR}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}};
-const int init_map_part2[MAX_RTG_TYPES_PART2][2] = {{FIRST_FLOOR, FIRST_FLOOR}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR + 1, FIRST_FLOOR + 2}, {FIRST_FLOOR, FIRST_FLOOR}, {FIRST_FLOOR, FIRST_FLOOR}};
-
-typedef struct {
-	int Gen;
-	int Chip;
-} RTG_PAIR;
-
-typedef struct {
-	RTG_PAIR map[MAX_RTG_TYPES];
-	int elevator;
-	int step_no;
-} HOUSE_MAP;
-
 int result1, result2;
-int real_rtg_types_count = MAX_RTG_TYPES_TEST;
 
 #define TEST 1
 
-void DescribeState(const HOUSE_MAP current_map) {
-	std::cout << std::endl;
-	for (int i = FINAL_FLOOR; i >= FIRST_FLOOR; i--) {
-		std::cout << 'F' << i << ' ';
-		if (current_map.elevator == i) {
-			std::cout << "E ";
-		} else {
-			std::cout << ". ";
-		}
-		for (int j = 0; j < real_rtg_types_count; j++) {
-			if (current_map.map[j].Gen == i) {
-				std::cout << (char)('A' + j) << type_names[GENERATOR].c_str();
-			} else {
-				std::cout << empty_name.c_str() << empty_type_name.c_str();
-			}
-
-			std::cout << ' ';
-
-			if (current_map.map[j].Chip == i) {
-				std::cout << (char)('A' + j) << type_names[MICROCHIP].c_str();
-			} else {
-				std::cout << empty_name.c_str() << empty_type_name.c_str();
-			}
-
-			std::cout << ' ';
-		}
-		std::cout << std::endl;
+class FuncCall {
+  public:
+	std::vector<int> n;
+	int k;
+	int e;
+	FuncCall(std::vector<int> n, int e, int k) {
+		this->n = std::vector<int>(n);
+		this->e = e;
+		this->k = k;
 	}
-	std::cout << std::endl;
-}
+};
 
-unsigned long long PackState(HOUSE_MAP state) {
-	unsigned long long result = 0;
-	unsigned char packed[MAX_RTG_TYPES];
+int DoIt(std::vector<int> n, long time_msec) {
+	std::chrono::steady_clock::time_point begin_time, end_time;
+	int result = -1;
 
-	// pack state
-	for (int i = 0; i < real_rtg_types_count; i++) {
-		packed[i] = (state.map[i].Gen & 0x0F);
-		packed[i] = packed[i] << 4;
-		packed[i] |= (state.map[i].Chip & 0x0F);
-	}
+	begin_time = std::chrono::steady_clock::now();
 
-	// order packed items
-	for (int i = 0; i < (real_rtg_types_count - 1); i++) {
-		if (packed[i] > packed[i + 1]) {
-			unsigned char tmp = packed[i];
-			packed[i] = packed[i + 1];
-			packed[i + 1] = tmp;
-		}
-	}
-
-	// pack all into one value
-	result = state.elevator;
-	for (int i = 0; i < real_rtg_types_count; i++) {
-		result = result << 8;
-		result |= packed[i];
-	}
+	end_time = std::chrono::steady_clock::now();
+	time_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time).count();
 
 	return result;
 }
 
-bool IsStateValid(const HOUSE_MAP current_map) {
-	bool is_gen, is_unpaired_chip;
-
-	for (int i = FIRST_FLOOR; i <= FINAL_FLOOR; i++) {
-		is_gen = is_unpaired_chip = false;
-
-		for (int j = 0; j < real_rtg_types_count; j++) {
-			if (current_map.map[j].Gen == i) {
-				is_gen = true;
-			}
-			if ((current_map.map[j].Chip == i) && (current_map.map[j].Gen != i)) {
-				is_unpaired_chip = true;
-			}
-		}
-		if (is_gen && is_unpaired_chip) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-void MapCopy(const HOUSE_MAP current_map, HOUSE_MAP &next_map) {
-	for (int j = 0; j < MAX_RTG_TYPES; j++) {
-		next_map.map[j] = current_map.map[j];
-	}
-	next_map.elevator = current_map.elevator;
-}
-
-void MoveItem(HOUSE_MAP &next_map, int rtg_item_type, int item_type, int new_floor) {
-	if (item_type == GENERATOR) {
-		next_map.map[rtg_item_type].Gen = new_floor;
-	} else {
-		next_map.map[rtg_item_type].Chip = new_floor;
-	}
-}
-
-bool IsInHistory(unsigned long long stamp, std::vector<unsigned long long> history) {
-	//return (std::find(history.begin(), history.end(), stamp) != history.end());
-	if (std::find(history.begin(), history.end(), stamp) != history.end()) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-int Explore2(HOUSE_MAP init_map) {
-	std::vector<unsigned long long> history;
-	HOUSE_MAP current_map, next, next2;
-	unsigned long long stamp, target;
-	int curr_floor_map[MAX_RTG_TYPES * 2][2];
-	int curr_floor_map_size;
-	std::queue<HOUSE_MAP> queue;
-
-	current_map.elevator = FINAL_FLOOR;
-	for (int j = 0; j < real_rtg_types_count; j++) {
-		current_map.map[j].Gen = FINAL_FLOOR;
-		current_map.map[j].Chip = FINAL_FLOOR;
-	}
-	target = PackState(current_map);
-
-	history.clear();
-	//queue.clear();
-	queue.push(init_map);
-
-	while (queue.size() > 0) {
-		current_map = queue.front();
-		queue.pop();
-		stamp = PackState(current_map);
-
-		if (stamp == target) {
-			return current_map.step_no;
-		}
-
-		if (IsInHistory(stamp, history)) {
-			continue;
-		} else {
-			history.push_back(stamp);
-		}
-
-		current_map.step_no++;
-
-		curr_floor_map_size = 0;
-		for (int j = 0; j < real_rtg_types_count; j++) {
-			if (current_map.map[j].Gen == current_map.elevator) {
-				curr_floor_map[curr_floor_map_size][0] = j;
-				curr_floor_map[curr_floor_map_size][1] = GENERATOR;
-				curr_floor_map_size++;
-			}
-			if (current_map.map[j].Chip == current_map.elevator) {
-				curr_floor_map[curr_floor_map_size][0] = j;
-				curr_floor_map[curr_floor_map_size][1] = MICROCHIP;
-				curr_floor_map_size++;
-			}
-		}
-
-		for (int i = 0; i < curr_floor_map_size; i++) {
-			if (current_map.elevator > FIRST_FLOOR) {
-				next = current_map;
-				next.elevator--;
-				MoveItem(next, curr_floor_map[i][0], curr_floor_map[i][1], next.elevator);
-				if (IsStateValid(next)) {
-					queue.push(next);
-				}
-				for (int j = i + 1; j < curr_floor_map_size; j++) {
-					next2 = next;
-					MoveItem(next2, curr_floor_map[j][0], curr_floor_map[j][1], next2.elevator);
-					if (IsStateValid(next2)) {
-						queue.push(next2);
-					}
-				}
-			}
-			if (current_map.elevator < FINAL_FLOOR) {
-				next = current_map;
-				next.elevator++;
-				MoveItem(next, curr_floor_map[i][0], curr_floor_map[i][1], next.elevator);
-				if (IsStateValid(next)) {
-					queue.push(next);
-				}
-				for (int j = i + 1; j < curr_floor_map_size; j++) {
-					next2 = next;
-					MoveItem(next2, curr_floor_map[j][0], curr_floor_map[j][1], next2.elevator);
-					if (IsStateValid(next2)) {
-						queue.push(next2);
-					}
-				}
-			}
-		}
-	}
-
-	return -1;
-}
-
 int main(void) {
-	HOUSE_MAP current_map;
-	std::vector<unsigned long long> optim;
 
 	std::cout << "=== Advent of Code 2016 - day 11 ====" << std::endl;
 
 #if TEST
 	std::cout << "--- test ---" << std::endl;
 
-	real_rtg_types_count = MAX_RTG_TYPES_TEST; //MAX_RTG_TYPES_PART1;
-	for (int j = 0; j < real_rtg_types_count; j++) {
-		current_map.map[j].Gen = init_map_test[j][GENERATOR];
-		current_map.map[j].Chip = init_map_test[j][MICROCHIP];
-	}
-	current_map.elevator = FIRST_FLOOR;
-	current_map.step_no = 0;
-	optim.clear();
-	//DescribeState(current_map);
-
-	result1 = Explore2(current_map);
 #endif
 
 	std::cout << "--- part 1 ---" << std::endl;
-
-	real_rtg_types_count = MAX_RTG_TYPES_PART1;
-	for (int j = 0; j < real_rtg_types_count; j++) {
-		current_map.map[j].Gen = init_map_part1[j][GENERATOR];
-		current_map.map[j].Chip = init_map_part1[j][MICROCHIP];
-	}
-	current_map.elevator = FIRST_FLOOR;
-	current_map.step_no = 0;
-	optim.clear();
-	//DescribeState(current_map);
-	result1 = Explore2(current_map);
-
-	real_rtg_types_count = MAX_RTG_TYPES_PART2;
-	for (int j = 0; j < real_rtg_types_count; j++) {
-		current_map.map[j].Gen = init_map_part2[j][GENERATOR];
-		current_map.map[j].Chip = init_map_part2[j][MICROCHIP];
-	}
-	current_map.elevator = FIRST_FLOOR;
-	current_map.step_no = 0;
-	optim.clear();
-	//DescribeState(current_map);
-	//result2 = Explore2(current_map);
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
