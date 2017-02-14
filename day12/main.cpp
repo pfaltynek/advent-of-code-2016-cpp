bc218430c1c1d6d292af169e15ba749ace0cf801
Faltynek Petr <FALTYNEK@zakladni.local>
2017-02-14 21:37:47 +0100

    finished solution for day 12

--- day12/main.cpp
+++ day12/main.cpp
@@ -9,12 +9,8 @@
 //#define TEST 1
 
 #ifdef TEST
-const int part1_val1 = 5;
-const int part1_val2 = 2;
 const std::string input_filename = "input-test.txt";
 #else
-const int part1_val1 = 61;
-const int part1_val2 = 17;
 const std::string input_filename = "input.txt";
 #endif
 
@@ -28,21 +24,20 @@ typedef enum {
 	INST_LD,
 	INST_INC,
 	INST_DEC,
-	INST_JNZ
+	INST_JNZ,
+	INST_JMP,
+	INST_NOP
 } INST_TYPE;
 
-typedef enum {
-	REG_NONE,
-	REG_A,
-	REG_B,
-	REG_C,
-	REG_D
-} REG_TYPE;
+#define REG_A_IDX 0
+#define REG_B_IDX 1
+#define REG_C_IDX 2
+#define REG_D_IDX 3
 
 typedef struct {
 	INST_TYPE inst_type;
 	int value;
-	REG_TYPE reg1, reg2;
+	int reg1, reg2;
 } INSTRUCTION;
 
 bool CheckInstruction(std::string instruction_line, std::string &instruction, std::string &operand1, std::string &operand2) {
@@ -69,22 +64,71 @@ bool CheckInstruction(std::string instruction_line, std::string &instruction, st
 	return false;
 }
 
-bool DecodeRegister(std::string operand, REG_TYPE &reg) {
-	reg = REG_TYPE::REG_NONE;
+void Trace(std::vector<INSTRUCTION> prg, int c_value, int &result1) {
+	int regs[4] = {0, 0, 0, 0};
+	int pc = 0;
+
+	regs[REG_C_IDX] = c_value;
+
+	while ((pc >= 0) && (pc < prg.size())) {
+		switch (prg[pc].inst_type) {
+			case INST_CP:
+				regs[prg[pc].reg2] = regs[prg[pc].reg1];
+				pc++;
+				break;
+
+			case INST_LD:
+				regs[prg[pc].reg1] = prg[pc].value;
+				pc++;
+				break;
+
+			case INST_INC:
+				regs[prg[pc].reg1]++;
+				pc++;
+				break;
+
+			case INST_DEC:
+				regs[prg[pc].reg1]--;
+				pc++;
+				break;
+
+			case INST_JNZ:
+				if (regs[prg[pc].reg1]) {
+					pc += prg[pc].value;
+				} else {
+					pc++;
+				}
+				break;
+
+			case INST_JMP:
+				pc += prg[pc].value;
+				break;
+
+			case INST_NOP:
+				pc++;
+				break;
+		}
+	}
+
+	result1 = regs[REG_A_IDX];
+}
+
+bool DecodeRegister(std::string operand, int &reg) {
+	reg = REG_A_IDX;
 
 	if (operand.size() == 1) {
 		switch (operand[0]) {
 			case 'a':
-				reg = REG_TYPE::REG_D;
+				reg = REG_A_IDX;
 				break;
 			case 'b':
-				reg = REG_TYPE::REG_D;
+				reg = REG_B_IDX;
 				break;
 			case 'c':
-				reg = REG_TYPE::REG_D;
+				reg = REG_C_IDX;
 				break;
 			case 'd':
-				reg = REG_TYPE::REG_D;
+				reg = REG_D_IDX;
 				break;
 			default:
 				return false;
@@ -119,41 +163,68 @@ int main(void) {
 		cnt++;
 		INSTRUCTION instruction;
 		instruction.value = 0;
-		instruction.reg1 = REG_TYPE::REG_NONE;
-		instruction.reg2 = REG_TYPE::REG_NONE;
+		instruction.reg1 = REG_A_IDX;
+		instruction.reg2 = REG_A_IDX;
 
 		if (!CheckInstruction(line, inst, op1, op2)) {
-			std::cout << "Invalid instruction at line" << cnt << std::endl;
+			std::cout << "Invalid instruction at line " << cnt << std::endl;
 			return -1;
 		}
-		
+
 		switch (inst[0]) {
 			case 'c':
-
+				if (DecodeRegister(op1, instruction.reg1)) {
+					instruction.inst_type = INST_CP;
+					if (!DecodeRegister(op2, instruction.reg2)) {
+						std::cout << "Invalid instruction \'cpy\' at line " << cnt << std::endl;
+						return -1;
+					}
+				} else {
+					instruction.inst_type = INST_LD;
+					instruction.value = atoi(op1.c_str());
+					if (!DecodeRegister(op2, instruction.reg1)) {
+						std::cout << "Invalid instruction \'cpy\' at line " << cnt << std::endl;
+						return -1;
+					}
+				}
 				break;
 			case 'i':
 				if (!DecodeRegister(op1, instruction.reg1)) {
-					std::cout << "Invalid \'inc \'instruction at line" << cnt << std::endl;
+					std::cout << "Invalid \'inc \'instruction at line " << cnt << std::endl;
 					return -1;
 				}
 				instruction.inst_type = INST_INC;
 				break;
 			case 'd':
 				if (!DecodeRegister(op1, instruction.reg1)) {
-					std::cout << "Invalid \'dec \'instruction at line" << cnt << std::endl;
+					std::cout << "Invalid \'dec \'instruction at line  " << cnt << std::endl;
 					return -1;
 				}
 				instruction.inst_type = INST_DEC;
 				break;
 			case 'j':
-
+				if (!DecodeRegister(op1, instruction.reg1)) {
+					if (atoi(op1.c_str())) {
+						instruction.inst_type = INST_JMP;
+						instruction.value = atoi(op2.c_str());
+					} else {
+						instruction.inst_type = INST_NOP;
+					}
+				} else {
+					instruction.inst_type = INST_JNZ;
+					instruction.value = atoi(op2.c_str());
+				}
 				break;
 		}
+		prg.push_back(instruction);
 	}
 
 	if (input.is_open()) {
 		input.close();
 	}
+
+	Trace(prg, 0, result1);
+	Trace(prg, 1, result2);
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
