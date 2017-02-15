ac4dcba1524695d342a2700567c7dde57b446a4e
Faltynek Petr <FALTYNEK@zakladni.local>
2017-02-15 20:08:41 +0100

    finished solution of day 13

---
+++ day13/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication {
+	public class Program {
+		public class Step {
+			public uint x, y, step;
+
+			public Step(uint x_init, uint y_init, uint step_init) {
+				x = x_init;
+				y = y_init;
+				step = step_init;
+			}
+		}
+		public static bool IsEvenParity(UInt32 v) {
+			v ^= v >> 1;
+			v ^= v >> 2;
+			v = (v & 0x11111111U) * 0x11111111U;
+			return ((v >> 28) & 1) != 1;
+		}
+
+		public static bool IsSpace(uint x, uint y, uint number) {
+			return IsEvenParity((x * x) + (3 * x) + (2 * x * y) + y + (y * y) + number);
+		}
+
+		private static Dictionary<uint, bool> map = new Dictionary<uint, bool>();
+		private static List<Step> queue = new List<Step>();
+
+		private static uint GetCoord(uint x, uint y) {
+			return (1000 * x) + y;
+		}
+
+		private static uint FindIt(uint startx, uint starty, uint targetx, uint targety, uint number) {
+			uint result = 0, coord;
+			uint target = GetCoord(targetx, targety);
+			queue.Add(new Step(startx, starty, 0));
+			Step step;
+			bool space;
+
+			while (queue.Count > 0) {
+				step = queue[0];
+				queue.RemoveAt(0);
+
+				coord = GetCoord(step.x, step.y);
+
+				if (coord == target) {
+					result = step.step;
+					break;
+				}
+
+				if (map.ContainsKey(coord)) {
+					continue;
+				}
+				space = IsSpace(step.x, step.y, number);
+				map.Add(coord, space);
+				if (space) {
+					if (step.x > 0) {
+						queue.Add(new Step(step.x - 1, step.y, step.step + 1));
+					}
+					if (step.y > 0) {
+						queue.Add(new Step(step.x, step.y - 1, step.step + 1));
+					}
+					queue.Add(new Step(step.x + 1, step.y, step.step + 1));
+					queue.Add(new Step(step.x, step.y + 1, step.step + 1));
+				}
+			}
+			return result;
+		}
+
+		public static void Main(string[] args) {
+			//uint test = FindIt(1, 1, 7, 4, 10);
+			uint result1 = FindIt(1, 1, 31, 39, 1364);
+		}
+
+		/*
+				public static bool IsEvenParity2(UInt32 x)
+				{
+
+					x ^= x >> 16;
+					x ^= x >> 8;
+					x ^= x >> 4;
+					x ^= x >> 2;
+					x ^= x >> 1;
+					return ((~x) & 1) == 1;
+				}
+		*/
+	}
+}
---
+++ day13/main.cpp
@@ -0,0 +1,161 @@
+
+#include <algorithm>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define TEST 1
+
+int result1, result2, test;
+
+class FuncCall {
+  public:
+	unsigned int x, y, step;
+	int k;
+	int e;
+	FuncCall(unsigned int x, unsigned int y, unsigned int step) {
+		this->x = x;
+		this->y = y;
+		this->step = step;
+	}
+};
+
+/*bool ComparePairs(Pair p1, Pair p2) {
+	if (p1.x != p2.x) {
+		return p1.x < p2.x;
+	}
+	return p1.y < p2.y;
+}*/
+
+bool IsEvenParity(unsigned int v) {
+	v ^= v >> 1;
+	v ^= v >> 2;
+	v = (v & 0x11111111U) * 0x11111111U;
+	return ((v >> 28) & 1) != 1;
+}
+
+bool IsSpace(unsigned int x, unsigned int y, unsigned int number) {
+	return IsEvenParity((x * x) + (3 * x) + (2 * x * y) + y + (y * y) + number);
+}
+
+unsigned int GetCoord(unsigned int x, unsigned int y) {
+	return (1000 * x) + y;
+}
+
+unsigned int DoIt_part1(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, unsigned int magic, long &time_msec) {
+	std::chrono::steady_clock::time_point begin_time, end_time;
+	unsigned int result = 0;
+	std::unordered_map<unsigned int, bool> map;
+	unsigned int target, coord;
+	std::queue<FuncCall> bfs;
+	bool space;
+
+	begin_time = std::chrono::steady_clock::now();
+
+	target = GetCoord(x2, y2);
+	FuncCall funccall(1, 1, 0);
+	bfs.push(funccall);
+	while (!bfs.empty()) {
+		FuncCall f = bfs.front();
+		bfs.pop();
+
+		coord = GetCoord(f.x, f.y);
+
+		if (coord == target) {
+			result = f.step;
+			break;
+		}
+
+		if (map.find(coord) != map.end())
+			continue;
+
+		space = IsSpace(f.x, f.y, magic);
+		map.emplace(coord, space);
+		if (space) {
+			if (f.x) {
+				bfs.push(FuncCall(f.x - 1, f.y, f.step + 1));
+			}
+			if (f.y) {
+				bfs.push(FuncCall(f.x, f.y - 1, f.step + 1));
+			}
+			bfs.push(FuncCall(f.x + 1, f.y, f.step + 1));
+			bfs.push(FuncCall(f.x, f.y + 1, f.step + 1));
+		}
+	}
+
+	end_time = std::chrono::steady_clock::now();
+	time_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time).count();
+
+	return result;
+}
+
+unsigned int DoIt_part2(unsigned int x1, unsigned int y1, unsigned int steps_max, unsigned int magic, long &time_msec) {
+	std::chrono::steady_clock::time_point begin_time, end_time;
+	unsigned int result = 0;
+	std::unordered_set<unsigned int> map;
+	unsigned int coord;
+	std::queue<FuncCall> bfs;
+	bool space;
+
+	begin_time = std::chrono::steady_clock::now();
+
+	FuncCall funccall(1, 1, 0);
+	bfs.push(funccall);
+	while (!bfs.empty()) {
+		FuncCall f = bfs.front();
+		bfs.pop();
+
+		coord = GetCoord(f.x, f.y);
+
+		if (f.step > steps_max) {
+			break;
+		}
+
+		if (map.find(coord) != map.end())
+			continue;
+
+		space = IsSpace(f.x, f.y, magic);
+		if (space)
+			result++;
+
+		map.emplace(coord);
+		if (space) {
+			if (f.x) {
+				bfs.push(FuncCall(f.x - 1, f.y, f.step + 1));
+			}
+			if (f.y) {
+				bfs.push(FuncCall(f.x, f.y - 1, f.step + 1));
+			}
+			bfs.push(FuncCall(f.x + 1, f.y, f.step + 1));
+			bfs.push(FuncCall(f.x, f.y + 1, f.step + 1));
+		}
+	}
+
+	end_time = std::chrono::steady_clock::now();
+	time_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time).count();
+
+	return result;
+}
+
+int main(void) {
+	long time = 0, t1 = 0, t2 = 0;
+	std::cout << "=== Advent of Code 2016 - day 11 ====" << std::endl;
+
+#if TEST
+	std::cout << "--- test ---" << std::endl;
+	test = DoIt_part1(1, 1, 7, 4, 10, time);
+	std::cout << "Result is " << test << " (" << time << "ms)" << std::endl;
+#endif
+
+	std::cout << "--- part 1 ---" << std::endl;
+	result1 = DoIt_part1(1, 1, 31, 39, 1364, t1);
+	std::cout << "Result is " << result1 << " (" << t1 << "ms)" << std::endl;
+
+	std::cout << "--- part 2 ---" << std::endl;
+	result2 = DoIt_part2(1, 1, 50, 1364, t2);
+	std::cout << "Result is " << result2 << " (" << t2 << "ms)" << std::endl;
+}
