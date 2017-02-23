f76319f225237790fc5a1ebf0ea740b90adee3c5
Petr Faltýnek <pfaltynek@gmail.com>
2017-02-23 12:48:10 +0100

    finished solution of day 19

--- day19/main.cpp
+++ day19/main.cpp
@@ -1,15 +1,55 @@
+#include <chrono>
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <list>
+#include <queue>
 #include <string>
 
-#include <list>
-
 //#define TEST1 1
+const int input = 3001330;
 
-int SolvePart1(int count) {
+int SolvePart2(int count, long &time_msec) {
+	std::chrono::steady_clock::time_point begin_time, end_time;
+	begin_time = std::chrono::steady_clock::now();
+	std::queue<int> thiefs, stolen;
+	int tmp;
+
+	begin_time = std::chrono::steady_clock::now();
+
+	for (int i = 0; i < count; i++) {
+		if (i < (count / 2)) {
+			thiefs.push(i + 1);
+		} else {
+			stolen.push(i + 1);
+		}
+	}
+
+	while ((thiefs.size() + stolen.size()) > 1) {
+		stolen.pop();
+		tmp = thiefs.front();
+		thiefs.pop();
+		stolen.push(tmp);
+		while ((thiefs.size() + 1) < stolen.size()) {
+			thiefs.push(stolen.front());
+			stolen.pop();
+		}
+	}
+
+
+	end_time = std::chrono::steady_clock::now();
+	time_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time).count();
+
+	return stolen.front();
+}
+
+int SolvePart1(int count, long &time_msec) {
+	std::chrono::steady_clock::time_point begin_time, end_time;
+	begin_time = std::chrono::steady_clock::now();
 	std::list<int> circle;
 	std::list<int>::iterator pos1, pos2;
+
+	begin_time = std::chrono::steady_clock::now();
 
 	for (int i = 0; i < count; i++) {
 		circle.emplace_back(i + 1);
@@ -30,24 +70,27 @@ int SolvePart1(int count) {
 		pos1++;
 	}
 
+	end_time = std::chrono::steady_clock::now();
+	time_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time).count();
+
 	return circle.front();
 }
 
 int main(void) {
 	int test, result1, result2;
-
-	int input = 3001330;
+	long ttest, t1, t2;
 
 	std::cout << "=== Advent of Code 2016 - day 19 ====" << std::endl;
 
 #if TEST1
-	test = SolvePart1(5);
+	test = SolvePart1(5, ttest);
+	test = SolvePart2(5, ttest);
 #endif
 
 	std::cout << "--- part 1 ---" << std::endl;
-	result1 = SolvePart1(input);
-	std::cout << "Result is " << result1 << std::endl;
+	result1 = SolvePart1(input, t1);
+	std::cout << "Result is " << result1 << " (" << t1 << "ms)" << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
-	result2 = 0;
-	std::cout << "Result is " << result2 << std::endl;
+	result2 = SolvePart2(input, t2);
+	std::cout << "Result is " << result2 << " (" << t2 << "ms)" << std::endl;
 }
