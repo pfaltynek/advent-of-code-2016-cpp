4fc46bfa7255848e984a249886327113b475d6bc
Petr Faltýnek <pfaltynek@gmail.com>
2017-03-01 15:06:50 +0100

    started solution of day 22 part 2

--- day22/main.cpp
+++ day22/main.cpp
@@ -26,19 +26,55 @@ bool DecodeNodeInfo(std::string line, DAY22_NODE &node) {
 	return false;
 }
 
-bool CompareNodesByUsed(DAY22_NODE node1, DAY22_NODE node2) {
-	return (node1.used > node2.used);
+int GetViableNodesPairCountBF(std::vector<DAY22_NODE> nodes) {
+	int cnt = 0;
+
+	for (int a = 0; a < nodes.size(); a++) {
+		for (int b = 0; b < nodes.size(); b++) {
+			if (a != b) {
+				if (nodes[a].used > 0) {
+					if (nodes[a].used <= nodes[b].avail) {
+						cnt++;
+					}
+				}
+			}
+		}
+	}
+
+	return cnt;
 }
 
-bool CompareNodesByAvail(DAY22_NODE node1, DAY22_NODE node2) {
-	return (node1.avail > node2.avail);
+void PrintMap(std::vector<DAY22_NODE> nodes, int x, int y, int max_avail) {
+	std::string line(x +1, ' ');
+	std::vector<std::string> disk_map;
+
+	disk_map.clear();
+	for (int i = 0; i <= y; i++) {
+		disk_map.push_back(line);
+	}
+
+	for (int i = 0; i < nodes.size(); i++) {
+		char symbol = '.';
+
+		if ((nodes[i].x == 0) && (nodes[i].y == 0)) {
+			symbol = 'S';
+		} else if ((nodes[i].x == x) && (nodes[i].y == 0)) {
+			symbol = 'T';
+		} else if (nodes[i].avail == max_avail) {
+			symbol = 'E';
+		} else if (nodes[i].used > max_avail) {
+			symbol = '#';
+		}
+
+		disk_map[nodes[i].y][nodes[i].x] = symbol;
+	}
+
+	for (int i = 0; i < disk_map.size(); i++) {
+		std::cout << disk_map[i].c_str() << std::endl;
+	}
 }
 
-int GetViableNodesPairCount(std::vector<DAY22_NODE> nodes) {
-	std::vector<DAY22_NODE> by_used(nodes), by_avail(nodes);
-
-	std::sort(by_used.begin(), by_used.end(), CompareNodesByUsed);
-	std::sort(by_avail.begin(), by_avail.end(), CompareNodesByAvail);
+int GetMovementSteps(std::vector<DAY22_NODE> nodes, int width, int height, int max_avail_x, int max_avail_y, int targ_x, int targ_y, int goal_x, int goal_y) {
 
 	return 0;
 }
@@ -47,7 +83,7 @@ int main(void) {
 	std::ifstream input;
 	std::string line;
 	std::vector<DAY22_NODE> data;
-	int cnt, result1, result2;
+	int cnt, result1, result2, x, y, max_avail, max_avail_x, max_avail_y;
 
 	cnt = 0;
 	result1 = 0;
@@ -63,11 +99,23 @@ int main(void) {
 	}
 
 	data.clear();
+	x = y = max_avail = max_avail_x = max_avail_y = 0;
 
 	while (std::getline(input, line)) {
 		DAY22_NODE node;
 		cnt++;
 		if (DecodeNodeInfo(line, node)) {
+			if (node.x > x) {
+				x = node.x;
+			}
+			if (node.y > y) {
+				y = node.y;
+			}
+			if (node.avail > max_avail) {
+				max_avail = node.avail;
+				max_avail_x = node.x;
+				max_avail_y = node.y;
+			}
 			data.push_back(node);
 		} else {
 			std::cout << "Line: '" << line << "' ignored." << std::endl;
@@ -79,9 +127,10 @@ int main(void) {
 	}
 
 	std::cout << "--- part 1 ---" << std::endl;
-	result1 = GetViableNodesPairCount(data);
+	result1 = GetViableNodesPairCountBF(data);
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
-
+	//PrintMap(data, x, y, max_avail);	// for manual solving of the part 2
+	result2 = GetMovementSteps(data, x + 1, y + 1, max_avail_x, max_avail_y, 0, 0, x, 0);
 	std::cout << "Result is " << result2 << std::endl;
 }
