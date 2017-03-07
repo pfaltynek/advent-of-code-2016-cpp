6659263336a48d29cfe495c2559bc17072a1cb29
Faltynek Petr <FALTYNEK@zakladni.local>
2017-03-07 21:40:33 +0100

    partially implemented part 2 of day 22

--- day22/main.cpp
+++ day22/main.cpp
@@ -1,13 +1,20 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <queue>
 #include <regex>
 #include <string>
-#include <vector>
+#include <unordered_set>
 
 typedef struct {
-	int x, y, size, used, avail, use_perc;
+	int x, y, size, used, avail, use_perc, step_cnt;
 } DAY22_NODE;
+
+typedef struct {
+	int x, y, step_cnt;
+	std::vector<int> path;
+} DAY22_STEP_INFO;
 
 bool DecodeNodeInfo(std::string line, DAY22_NODE &node) {
 	std::regex node_info_regex("\\/dev\\/grid\\/node-x(\\d+)-y(\\d+)[[:blank:]]+(\\d+)T[[:blank:]]+(\\d+)T[[:blank:]]+(\\d+)T[[:blank:]]+(\\d+)%$");
@@ -44,7 +51,7 @@ int GetViableNodesPairCountBF(std::vector<DAY22_NODE> nodes) {
 }
 
 void PrintMap(std::vector<DAY22_NODE> nodes, int x, int y, int max_avail) {
-	std::string line(x +1, ' ');
+	std::string line(x + 1, ' ');
 	std::vector<std::string> disk_map;
 
 	disk_map.clear();
@@ -69,12 +76,94 @@ void PrintMap(std::vector<DAY22_NODE> nodes, int x, int y, int max_avail) {
 	}
 
 	for (int i = 0; i < disk_map.size(); i++) {
-		std::cout << disk_map[i].c_str() << std::endl;
+		std::cout << disk_map[i].c_str() << "||" << std::endl;
 	}
 }
 
-int GetMovementSteps(std::vector<DAY22_NODE> nodes, int width, int height, int max_avail_x, int max_avail_y, int targ_x, int targ_y, int goal_x, int goal_y) {
+int GetCoord(int x, int y) {
+	//return ((x & 0xFFFF) << 16) | (y & 0xFFFF);
+	return (x * 1000) + y;
+}
+/*
+void DecodeCoord(int coord, int &x, int &y) {
+	x = (coord >> 16) & 0xFFFF;
+	y = coord & 0xFFFF;
+}
+*/
+int GetMovementStepsToBringEmptyDiscBeforeTagetData(std::map<int, DAY22_NODE> map, int empty_x, int empty_y, int target_x, int target_y, int max_x, int max_y) {
+	int target, coord, avail;
+	std::unordered_set<int> history;
+	std::queue<DAY22_STEP_INFO> q;
+	DAY22_STEP_INFO dsi, tmp;
+
+	avail = map[GetCoord(empty_x, empty_y)].avail;
+
+	dsi.x = empty_x;
+	dsi.y = empty_y;
+	dsi.step_cnt = 0;
+	q.push(dsi);
+
+	target = GetCoord(target_x, target_y);
+
+	while (!q.empty()) {
+		dsi = q.front();
+		q.pop();
+		if ((dsi.x < 0) || (dsi.y < 0) || (dsi.x > max_x) || (dsi.y > max_y)) {
+			continue;
+		}
+		coord = GetCoord(dsi.x, dsi.y);
+
+		if (coord == target) {
+			return dsi.step_cnt;
+		}
+
+		if (history.find(coord) != history.end()) {
+			continue;
+		}
+
+		history.emplace(coord);
+
+		if (map[coord].used > avail) {
+			continue;
+		}
+
+		dsi.step_cnt++;
+		dsi.path.push_back(coord);
+		tmp = dsi;
+		dsi.x++;
+		q.push(dsi);
+		dsi = tmp;
+		dsi.x--;
+		q.push(dsi);
+		dsi = tmp;
+		dsi.y++;
+		q.push(dsi);
+		dsi = tmp;
+		dsi.y--;
+		q.push(dsi);
+	}
+
+	return -1;
+}
+
+int GetDancingAroundStepsCount(std::map<int, DAY22_NODE> map, int max_x, int max_y, int free_x, int free_y, int targ_x, int targ_y, int final_x, int final_y) {
+
 	return 0;
+}
+
+int GetMovementSteps(std::vector<DAY22_NODE> nodes, int max_x, int max_y, int max_avail_x, int max_avail_y) {
+	std::map<int, DAY22_NODE> map;
+	int steps = 0;
+
+	map.clear();
+	for (int i = 0; i < nodes.size(); i++) {
+		map.emplace(GetCoord(nodes[i].x, nodes[i].y), nodes[i]);
+	}
+
+	steps += GetMovementStepsToBringEmptyDiscBeforeTagetData(map, max_avail_x, max_avail_y, max_x - 1, 0, max_x, max_y);
+	steps += GetDancingAroundStepsCount(map, max_x, max_y, max_x - 1, 0, max_x, max_y, 0, 0);
+
+	return steps;
 }
 
 int main(void) {
@@ -128,7 +217,7 @@ int main(void) {
 	result1 = GetViableNodesPairCountBF(data);
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
-	//PrintMap(data, x, y, max_avail);	// for manual solving of the part 2
-	result2 = GetMovementSteps(data, x + 1, y + 1, max_avail_x, max_avail_y, 0, 0, x, 0);
+	//PrintMap(data, x, y, max_avail); // for manual solving of the part 2
+	result2 = GetMovementSteps(data, x, y, max_avail_x, max_avail_y);
 	std::cout << "Result is " << result2 << std::endl;
 }
